15cb121065a96ace4e88b7f0698a878e7237521c
izzy0800 <stemson.bella@hotmail.com>
2025-10-01 01:05:32 +1300

    Revert "kms"
    This reverts commit a5c16ef161dd373ce54693968d09f1c6d9ca16b2.

---
+++ Assets/IsabellaStemson/NPCS/Block.cs
@@ -0,0 +1,48 @@
+using System.Collections;
+using System.Collections.Generic;
+using Unity.VisualScripting;
+using UnityEngine;
+
+public class Block : MonoBehaviour
+{
+    //public int correctIndex;
+    public Vector2Int Position;
+    public Vector2Int Size = Vector2Int.one;
+    public bool isGoal;
+    private Slider_Puzzle_Manager manager;
+
+    public bool MovesHorizontally => Size.x > Size.y;
+    public bool MovesVertically => Size.y > Size.x;
+
+    public Vector2Int GetBlockSize()
+    {
+        return Size;
+    }
+
+    private void Start()
+    {
+        manager = Slider_Puzzle_Manager.Instance;
+    }
+
+    public void UpdateGridPosition(Vector2Int newPosition)
+    {
+        Position = newPosition;
+        //Debug.Log($"{gameObject.name} moved to grid position: {Position}");
+    }
+
+    public List<Vector2Int> GetOccupiedCells(Vector2Int? overridePos = null)
+    {
+        Vector2Int basePos = overridePos ?? Position;
+        List<Vector2Int> cells = new List<Vector2Int>();
+
+        for (int x = 0; x <Size.x; x++)
+        {
+            for (int y = 0; y <Size.y; y++)
+            {
+                cells.Add(new Vector2Int(basePos.x + x, basePos.y + y));
+            }
+        }
+        return cells;
+    }
+
+}
---
+++ Assets/IsabellaStemson/NPCS/DraggableBlock.cs
@@ -0,0 +1,104 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.EventSystems;
+
+public class DraggableBlock : MonoBehaviour, IBeginDragHandler, IDragHandler, IEndDragHandler, IPointerEnterHandler
+{
+
+    private RectTransform rectTransform;
+    private Canvas canvas;
+    private Vector2 originalPos;
+
+
+    private void Awake()
+    {
+        rectTransform = GetComponent<RectTransform>();
+        canvas = GetComponentInParent<Canvas>();
+    }
+
+    
+
+    public void OnBeginDrag(PointerEventData eventData)
+    {
+        originalPos = rectTransform.anchoredPosition;
+    }
+
+    public void OnDrag(PointerEventData eventData)
+    {
+        Block blockComponent = GetComponent<Block>();
+        Vector2 delta = eventData.delta / canvas.scaleFactor;
+
+        if (blockComponent != null)
+        {
+            if (blockComponent.MovesHorizontally)
+            {
+                delta.y = 0;
+            }
+            else if (blockComponent.MovesVertically)
+            {
+                delta.x = 0;
+            }
+        }
+
+        rectTransform.anchoredPosition += delta;
+
+        Slider_Puzzle_Manager.Instance.goalBlock.GetComponent<BoxCollider2D>().enabled = false;
+    }
+
+    public void OnEndDrag(PointerEventData eventData)
+    {
+        //Vector2 originalAnchoredPos = originalPos;
+        bool isValidMove;
+        Vector2 snapped = Slider_Puzzle_Manager.Instance.GetNearestCellPositionWithCollision(rectTransform, out isValidMove);
+
+        if (isValidMove)
+        {
+            rectTransform.anchoredPosition = snapped;
+            Debug.Log("Valid move applied");
+
+            Slider_Puzzle_Manager.Instance.goalBlock.GetComponent<BoxCollider2D>().enabled = true;
+        }
+        else
+        {
+            rectTransform.anchoredPosition = originalPos;
+            Debug.Log("Invalid move - returned to original position");
+        }
+
+        //Block blockComponent = GetComponent<Block>();
+        //if (blockComponent != null)
+        //{
+        //    Vector2 relativePos = snapped - new Vector2(-342.30f, 426.40f);
+        //    Vector2 totalCellSize = new Vector2(137.5f, 171.1f); //wrong maybe?
+
+        //    int targetX = Mathf.RoundToInt(relativePos.x / totalCellSize.x);
+        //    int targetY = Mathf.RoundToInt(-relativePos.y / totalCellSize.y);
+        //    Vector2Int targetGridPos = new Vector2Int(targetX, targetY);
+
+        //    if (Slider_Puzzle_Manager.Instance.IsPositionValid(blockComponent, targetGridPos))
+        //    {
+        //        rectTransform.anchoredPosition = snapped;
+        //        blockComponent.UpdateGridPosition(targetGridPos);
+        //        Debug.Log($"{blockComponent.name} move to valid position {targetGridPos}");
+        //    }
+        //    else
+        //    {
+        //        rectTransform.anchoredPosition = originalPos;
+        //        Debug.Log($"{blockComponent.name} blocked by collision, staying at {blockComponent.Position}");
+        //    }
+        //}
+        //else
+        //{
+        //    rectTransform.anchoredPosition = snapped;
+        //}
+
+        Slider_Puzzle_Manager.Instance.UpdateGrid();
+
+    }
+
+    public void OnPointerEnter(PointerEventData eventData)
+    {
+        //Debug.Log("IM A BLOCK");
+        //MAKE SURE THAT THE CANVAS 'BLOCKS RAYCASTS' IS TICKED FOR SOME REASON
+    }
+}
