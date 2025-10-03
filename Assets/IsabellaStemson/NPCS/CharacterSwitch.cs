b3fbcdf122a862b1fad5b256ac394fa9ea8f78ba
izzy0800 <stemson.bella@hotmail.com>
2025-10-03 23:05:57 +1300

    fixed camera broke asf

--- Assets/IsabellaStemson/NPCS/CharacterSwitch.cs
+++ Assets/IsabellaStemson/NPCS/CharacterSwitch.cs
@@ -2,6 +2,7 @@ using System;
 using System.Collections;
 using System.Collections.Generic;
 using System.Runtime.ConstrainedExecution;
+using Cinemachine;
 using UnityEngine;
 using UnityEngine.ProBuilder.MeshOperations;
 
@@ -9,15 +10,12 @@ public class CharacterSwitch : CryptidUtils
 {
     public GameObject player;
     public GameObject npc;
-
     ParticleSystem playerParticles;
 
-    public ThirdPersonCamera thirdPerson;
-    public FirstPersonCamera firstPerson;
+    public CinemachineVirtualCamera firstPersonCam;
 
     private bool isControllingNPC = false;
     public bool IsPossessing => isControllingNPC;
-
     PlayerMovement playerMovement;
 
     private enum TargetType
@@ -26,11 +24,17 @@ public class CharacterSwitch : CryptidUtils
         npc
     }
    
-    // Start is called before the first frame update
     void Start()
     {
         playerMovement = player.GetComponent<PlayerMovement>();
         playerParticles = player.GetComponentInChildren<ParticleSystem>();
+        if (firstPersonCam != null)
+        {
+            firstPersonCam.Priority = 0;
+            firstPersonCam.Follow = null;
+            firstPersonCam.LookAt = null;
+        }
+
         SwitchToPlayer();
     }
 
@@ -50,7 +54,6 @@ public class CharacterSwitch : CryptidUtils
         bool isNPC = type == TargetType.npc;
         isControllingNPC = isNPC;
 
-       
         if (npc != null)
         {
             var npcMovement = npc.GetComponent<PlayerMovement>();
@@ -59,40 +62,85 @@ public class CharacterSwitch : CryptidUtils
                 npcMovement.allowVerticalMovement = false;
             }
 
-            //to toggle sprite visblility
             var npcScript = npc.GetComponent<NPCscript>();
             if (npcScript != null)
             {
                 npcScript.SetSpriteVisible(!isNPC);
-                firstPerson.targetOffset = new Vector3(0, npcScript.height, 0);
+
+                if (isNPC && firstPersonCam != null)
+                {
+                    Transform cameraMount = npc.transform.Find("CameraMount");
+                    if (cameraMount == null)
+                    {
+                        GameObject mountObj = new GameObject("CameraMount");
+                        mountObj.transform.SetParent(npc.transform);
+                        mountObj.transform.localPosition = new Vector3(0, npcScript.height, 0.2f);
+                        mountObj.transform.localRotation = Quaternion.identity;
+                        cameraMount = mountObj.transform;
+                    }
+
+                    firstPersonCam.Follow = cameraMount;
+
+                    GameObject lookTarget = GameObject.Find("FirstPersonLookTarget");
+                    if (lookTarget == null)
+                    {
+                        lookTarget = new GameObject("FirstPersonLookTarget");
+                    }
+                    lookTarget.transform.SetParent(cameraMount);
+                    lookTarget.transform.localPosition = new Vector3(0, 0, 5f); 
+                    lookTarget.transform.localRotation = Quaternion.identity;
+
+                    firstPersonCam.LookAt = lookTarget.transform;
+
+                    DisableAllZoneCameras();
+
+                    firstPersonCam.Priority = 20;
+
+                    Debug.Log($"First-person camera following: {cameraMount.name} at position {cameraMount.position}");
+                }
+            }
+        }
+
+        if (!isNPC)
+        {
+            if (firstPersonCam != null)
+            {
+                firstPersonCam.Priority = 0;
+                firstPersonCam.Follow = null;
+                firstPersonCam.LookAt = null;
+
+                GameObject lookTarget = GameObject.Find("FirstPersonLookTarget");
+                if (lookTarget != null)
+                {
+                    Destroy(lookTarget);
+                }
             }
 
+            ReactivateZoneCameraForPlayer();
         }
 
         EnableControl(player, !isNPC);
         if (npc != null)
             EnableControl(npc, isNPC);
 
-        thirdPerson.enabled = !isNPC;
-        firstPerson.enabled = isNPC;
-
-        thirdPerson.target = player;
-        firstPerson.target = npc;
-
         playerMovement.allowVerticalMovement = !isNPC;
         playerParticles.gameObject.SetActive(!isNPC);
-
-      
-
     }
 
-    // Update is called once per frame
     private void Update()
     {
         //Pressing 'Tab' will toggle back to the player
         if (isControllingNPC && Input.GetKeyDown(KeyCode.Tab))
         {
             Switch(TargetType.player);
+        }
+
+        if (Input.GetKeyDown(KeyCode.F1))
+        {
+            Debug.Log($"Is Possessing: {IsPossessing}");
+            Debug.Log($"FirstPersonCam Priority: {firstPersonCam.Priority}");
+            Debug.Log($"FirstPersonCam Follow: {firstPersonCam.Follow}");
+            Debug.Log($"FirstPersonCam Position: {firstPersonCam.transform.position}");
         }
     }
 
@@ -103,12 +151,38 @@ public class CharacterSwitch : CryptidUtils
 
         if (character.GetComponent<NPCscript>())
         {
-            //character.GetComponent<NPCscript>().enabled = isEnabled;
             character.GetComponent<NPCscript>().col.isTrigger = !isEnabled;
         }
 
         if (character.GetComponent<PickUpSystem>())
             character.GetComponent<PickUpSystem>().enabled = isEnabled;
+    }
 
+    private void DisableAllZoneCameras()
+    {
+        CamSwitcher[] allSwitchers = FindObjectsOfType<CamSwitcher>();
+        foreach (var switcher in allSwitchers)
+        {
+            if (switcher.assignedCamera != null)
+            {
+                switcher.assignedCamera.Priority = 0;
+            }
+        }
+        Debug.Log("Disabled all zone cameras for possession");
+    }
+
+    private void ReactivateZoneCameraForPlayer()
+    {
+        CamSwitcher[] allSwitchers = FindObjectsOfType<CamSwitcher>();
+        foreach (var switcher in allSwitchers)
+        {
+            Collider zoneCollider = switcher.GetComponent<Collider>();
+            if (zoneCollider != null && zoneCollider.bounds.Contains(player.transform.position))
+            {
+                switcher.assignedCamera.Priority = 10;
+                Debug.Log($"Reactivated camera for zone: {switcher.gameObject.name}");
+                break;
+            }
+        }
     }
 }
--- Assets/IsabellaStemson/environment/CamSwitcher.cs
+++ Assets/IsabellaStemson/environment/CamSwitcher.cs
@@ -5,21 +5,78 @@ using Cinemachine;
 
 public class CamSwitcher : MonoBehaviour
 {
-    public Transform Player;
-    public CinemachineVirtualCamera activeCam;
-    
-    private void OnTriggerEnter(Collider other) { 
-    if(other.CompareTag("Player"))
+    public CinemachineVirtualCamera assignedCamera;
+
+    private void Start()
+    {
+        if (assignedCamera == null)
         {
-            activeCam.Priority = 1;
+            Debug.LogError($"No camera assigned to CamSwitcher on {gameObject.name}!");
         }
-    }
-    private void OnTriggerExit(Collider other) { 
-    if (other.CompareTag("Player"))
+
+        Collider col = GetComponent<Collider>();
+        if (col == null || !col.isTrigger)
         {
-            activeCam.Priority = 0;
+            Debug.LogError($"CamSwitcher on {gameObject.name} needs a trigger collider!");
         }
     }
 
+    private void OnTriggerEnter(Collider other)
+    {
+        if (other.CompareTag("Player"))
+        {
+            Debug.Log("Player entered camera zone: " + gameObject.name);
+
+            CharacterSwitch charSwitch = FindObjectOfType<CharacterSwitch>();
+            if (charSwitch != null && charSwitch.IsPossessing)
+            {
+                Debug.Log("Not switching camera - currently possessing NPC");
+                return;
+            }
+
+            CamSwitcher[] allSwitchers = FindObjectsOfType<CamSwitcher>();
+            foreach (var switcher in allSwitchers)
+            {
+                if (switcher != this && switcher.assignedCamera != null)
+                {
+                    switcher.assignedCamera.Priority = 0;
+                }
+            }
+
+            assignedCamera.Priority = 10;
+            Debug.Log("Set camera priority to 10");
+        }
+    }
+
+    private void OnTriggerExit(Collider other)
+    {
+        if (other.CompareTag("Player"))
+        {
+            Debug.Log("Player exited camera zone: " + gameObject.name);
+
+            CharacterSwitch charSwitch = FindObjectOfType<CharacterSwitch>();
+            if (charSwitch != null && charSwitch.IsPossessing)
+            {
+                return;
+            }
+
+            assignedCamera.Priority = 0;
+            Debug.Log("Set camera priority to 0");
+        }
+    }
+
+    private void OnDrawGizmos()
+    {
+        Collider col = GetComponent<Collider>();
+        if (col != null)
+        {
+            Gizmos.color = new Color(1, 1, 0, 0.3f); 
+            BoxCollider box = col as BoxCollider;
+            if (box != null)
+            {
+                Gizmos.DrawWireCube(transform.position + box.center, box.size);
+            }
+        }
+    }
 }
 
